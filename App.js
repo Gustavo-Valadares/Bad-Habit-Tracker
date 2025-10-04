cd1f1fd9bfdf8c27317f694be4bc7992d833a4f6
Gustavo-Valadares <gustavoh.valadares@outlook.com>
2025-10-04 18:28:53 -0300

    Adição do recurso de criação de hábitos e estilização do menu modal

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Pressable, Modal } from 'react-native';
+import { StyleSheet, Text, TextInput, View, FlatList, Alert, Pressable, Modal } from 'react-native';
 import { SafeAreaView, ListRenderItemInfo } from 'react-native-safe-area-context';
 import React, {useState} from 'react';
 
@@ -12,37 +12,38 @@ export default function App() {
   // }
   
   const Default_Habits = [
-    {id: "1", nome: `Hábito`, count: 0},
-    {id: "2", nome: "Hábito", count: 0},
-    {id: "3", nome: "Hábito", count: 0},
-    {id: "4", nome: "Hábito", count: 0},
-    {id: "5", nome: "Hábito", count: 0},
-    {id: "6", nome: "Hábito", count: 0},
-    {id: "7", nome: "Hábito", count: 0},
-    {id: "8", nome: "Hábito", count: 0},
-    {id: "9", nome: "Hábito", count: 0},
-    {id: "10", nome: "Hábito", count: 0},
-    {id: "11", nome: "Hábito", count: 0},
-    {id: "12", nome: "Hábito", count: 0},
-    {id: "13", nome: "Hábito", count: 0},
-    {id: "14", nome: "Hábito", count: 0},
-    {id: "15", nome: "Hábito", count: 0},
 
-    /*
-    {id: "1", nome: "Comi Fritura", count: 0},
-    {id: "2", nome: "Pulei Refeição", count: 0},
-    {id: "3", nome: "Comi Doce", count: 0},
-    {id: "4", nome: "Faltei Academia", count: 0},
-    */
+
   ]
 
-  const [Habitos, setHabitos] = useState(Default_Habits); //vairável useState 
+  const [ListaDeHabitos, setListaDeHabitos] = useState(Default_Habits); //vairável useState para a list de ListaDeHabitos
+
+  const [novoHabito, setNovoHabito] = useState(''); // variável para setar um novo habito
 
   const [modalVisivel, setModalVisivel] = useState(false); //variável useState do modal que controla o aparecimento do modal
 
-  const itemRender = ({item}) => (
+  const adicionarHabito = () => {
+    if(novoHabito.trim() === ''){ //.thim() é um método que tira espaços antes e depois da string. se não há nenhum caracter além de espaço irá ser uma string vazia (tratamento de excessão)
+      Alert.alert("Digite o nome do hábito para adicioná-lo")
+      return;
+    }
+
+    const novoItem = {
+      id: String(Date.now()), // gera um id único para cada hábito
+      nome: novoHabito,
+      count: 0,
+    }
+
+    setListaDeHabitos([novoItem, ...ListaDeHabitos]); //...ListadeHabitos cria uma nova lista com o hábito antigos e adiciona novoItem a ele
+
+    setNovoHabito(''); // limpa o campo de input
+
+    setModalVisivel(false); // fecha o modal
+  };
+
+  const itemRender = ({item}) => ( // função que rederiza os itens do flatList
     <View style={styles.itens}>
-      <Text style={styles.item}>{item.nome} {item.id}: {item.count}</Text>
+      <Text style={styles.item}>{item.nome}: {item.count}</Text>
     </View>
   );
 
@@ -50,37 +51,11 @@ export default function App() {
     <View style={{flex: 1}}> {/*necessário colocar a tag view para implemetar o botão absoluto da págia (bot~
     ao de adicionar novo item na lista)*/}
         <SafeAreaView style={styles.container}>
-          
-          <View style={{}}>
-            <Modal 
-              visible={modalVisivel}
-              transparent={true}
-              animationType="fade"
-              onRequestClose={() => setModalVisivel(false)} // permite fechar o menu modal com o borão voltar do android
-            > 
-
-            <View style={styles.modalContainer}>
-              <View style={styles.modalContent}>
-                  <Text style={{color: 'white', alignSelf: 'center'}}> Menu Modal Aqui</Text>
-
-                <Pressable 
-                  style={styles.closeModalButton}
-                  onPress={() => setModalVisivel(false)}
-                >
-                  <Text style={{color: 'red'}}>Fechar</Text>
-                </Pressable>
-
-              </View>
-            </View>
-
-
-            </Modal>
-          </View>
 
 
           <Text style={styles.textStyle}>Bad Habit Tracker</Text>
             <FlatList style={styles.listContainer}
-              data={Habitos}
+              data={ListaDeHabitos}
               renderItem={itemRender}
               keyExtractor={item => item.id}
           />
@@ -92,11 +67,57 @@ export default function App() {
           </Pressable>
 
           <Pressable style={styles.update}
-            onPress={() => setHabitos(Default_Habits)}
+            onPress={() => setListaDeHabitos(Default_Habits)}
           >
             <Text>Atualizar Lista</Text>
           </Pressable>
         <StatusBar style="auto" /> {/*controla a barra de status nativa do dispositivo (Hora, Nível da bateria, Sinal de Wi-Fi e de rede celular, Ícones de notificação) */}
+
+
+
+          <View style={{}}>
+            <Modal 
+              visible={modalVisivel}
+              transparent={true}
+              animationType="fade"
+              onRequestClose={() => setModalVisivel(false)} // permite fechar o menu modal com o borão voltar do android
+            > 
+
+            <View style={styles.modalContainer}>
+              <View style={styles.modalContent}>
+                  <Text style={{color: '#ebebebff', alignSelf: 'center', marginBottom: 20, marginTop: 10, fontSize: 16}}> Adicionar Hábito Novo</Text>
+
+                  <TextInput
+                    style={styles.newHabitInput}
+                    placeholder="Ex: Comer doce"
+                    placeholderTextColor='#a4a4a46e'
+                    value={novoHabito}
+                    onChangeText={setNovoHabito}
+                  >
+                  </TextInput>
+                
+                  <View style={{flexDirection: 'row', borderWidth: 0, borderColor: 'yellow', alignSelf: 'center', marginTop: 15, paddingHorizontal: 10,}}>
+                    <Pressable 
+                      style={styles.closeModalButton}
+                      onPress={() => setModalVisivel(false)}
+                    >
+                      <Text style={{color: 'red', padding: 5, paddingHorizontal: 10}}>Fechar</Text>
+                    </Pressable>
+
+                    <Pressable 
+                      style={styles.confirmModalButton}
+                      onPress={() => adicionarHabito()}
+                    >
+                      <Text style={{color: 'green'}}>Adicionar</Text>
+                    </Pressable>
+                    
+                  </View>
+              </View>
+            </View>
+
+
+            </Modal>
+          </View>
 
       </SafeAreaView>
     </View>
@@ -105,11 +126,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  title: {
-    backgroundColor: '#8d3535ff',
-    boderWidth: 1,
-  
-  },
 
   listContainer: {
     color: 'white',
@@ -131,7 +147,7 @@ const styles = StyleSheet.create({
   },
 
   textStyle: {
-    color: 'white',
+    color: '#ebebebff',
     fontSize: 25,
     backgroundColor: '#302d2dff',
     paddingTop: 15,
@@ -142,26 +158,34 @@ const styles = StyleSheet.create({
 
   modalContent: {
     width: '80%',
-    backgroundColor: 'black',
-    opacity: 0.85,
+    backgroundColor: '#313131ff',
+    opacity: 0.90,
     paddingHorizonal: 50,
-    paddingBottom: 50,
-    magin: 40,
+    paddingBottom: 40,
+    borderRadius: 10,
   },
 
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+
+  },
+
+  confirmModalButton: {
+    borderWidth: 1, 
+    borderColor: 'green', 
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    borderRadius: 25,
+    marginLeft: 60,
   },
 
   closeModalButton: {
-    justifyContent: 'right',
     borderWidth: 1,
     borderColor: 'red',
     borderRadius: 25,
-    marginTop: 35,
-    alignItems: 'center',
+    marginRight: 60,
 
   },
 
@@ -176,18 +200,30 @@ const styles = StyleSheet.create({
     borderColor: 'green',
   },
 
+  newHabitInput: {
+    borderWidth: 1,
+    borderColor: '#c0c0c0ff',
+    borderRadius: 10,
+    color: '#c0c0c0ff',
+    marginHorizontal: 20,
+    marginBottom: 24,
+    paddingLeft: 12,
+
+    
+  },
+
   update: {
     position: 'absolute',
     right: 60,
     bottom: 50,
     borderWidth: 1,
     borderRadius: 25,
+    padding: 10,
   },
 
   container: {
     flex: 1,
     backgroundColor: '#d0d0d0ff',
-    boderWidth: 1,
     borderColor: 'black',
     
   },
